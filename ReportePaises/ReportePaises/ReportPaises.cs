7dbb149b52604eae27ee390be5c82855aff18a98
Jorgeone1 <jorgeifpdam@gmail.com>
2024-02-02 22:24:36 +0100

    mas cosas

--- ReportePaises/ReportePaises/ReportPaises.cs
+++ ReportePaises/ReportePaises/ReportPaises.cs
@@ -8,5 +8,9 @@ namespace ReportePaises
 {
     internal class ReportPaises
     {
+        public String code { get; set; }
+        public String name { get; set; }
+        public String continent { get; set; }
+        public ReportPaises() { }
     }
 }
