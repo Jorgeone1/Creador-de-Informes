ed666a10b4e3ed0a8c32a8e072280850e3692445
Jorgeone1 <jorgeifpdam@gmail.com>
2024-02-06 22:28:08 +0100

    imagen

--- ReportePaises/ReportePaises/Form1.Designer.cs
+++ ReportePaises/ReportePaises/Form1.Designer.cs
@@ -33,10 +33,10 @@
             // 
             // reportViewer1
             // 
-            this.reportViewer1.Location = new System.Drawing.Point(12, 81);
+            this.reportViewer1.Location = new System.Drawing.Point(12, 12);
             this.reportViewer1.Name = "reportViewer1";
             this.reportViewer1.ServerReport.BearerToken = null;
-            this.reportViewer1.Size = new System.Drawing.Size(761, 338);
+            this.reportViewer1.Size = new System.Drawing.Size(776, 426);
             this.reportViewer1.TabIndex = 0;
             // 
             // Form1
