0135ca1a57e4eaeff3e2e74db3ad83952a965ea7
Jorgeone1 <jorgeifpdam@gmail.com>
2024-02-08 01:42:50 +0100

    comentarios

--- ReportePaises/ReportePaises/Form1.cs
+++ ReportePaises/ReportePaises/Form1.cs
@@ -12,26 +12,41 @@ using MySql.Data;
 using MySql.Data.MySqlClient;
 namespace ReportePaises
 {
+    /// <summary>
+    /// Clase parcial del formulario
+    /// </summary>
     public partial class Form1 : Form
     {
+        /// <summary>
+        /// metodo que inicia los componentes.
+        /// </summary>
         public Form1()
         {
             InitializeComponent();
         }
-
+        /// <summary>
+        /// metodo que carga el formulario con su compenentes. En este caso rellenar los datos dentro del rdlc.
+        /// </summary>
+        /// <param name="sender">El objeto que desencadenó el evento de carga del formulario.</param>
+        /// <param name="e">Los argumentos del evento de carga del formulario.</param>
         private void Form1_Load(object sender, EventArgs e)
         {
-            String connstr = "server=localhost;user=root;password='';database=world;port=3306";
-            MySqlConnection conn = new MySqlConnection(connstr);
+            MySqlConnection conn = getConnection();
             try
             {
-                conn.Open();
+                conn.Open();// abre la conexion
+
+                //inserta el select dentro del MySqlCommand.
                 String sql = "Select code, name, continent From country";
                 MySqlCommand cmd = new MySqlCommand();
                 cmd.Connection = conn;
                 cmd.CommandText = sql;
+
+                //crea un MySqlReader y crea una lista para guardar los datos del select
                 MySqlDataReader rdr = cmd.ExecuteReader();
                 List<ReportPaises> lrp = new List<ReportPaises>();
+
+                //crea un bucle while en donde leera cada columna y lo introducira al array o lista
                 while (rdr.Read())
                 {
                     ReportPaises rp = new ReportPaises();
@@ -41,18 +56,34 @@ namespace ReportePaises
                     lrp.Add(rp);
                     rp = null;
                 }
+                //cierra la conexion
                 rdr.Close();
+
+                //creamos un objeto con reportDataSource la cuales contiene las tablas y diseño. Ademas de pasarle tambien los datos.
                 ReportDataSource rds = new ReportDataSource("ReportePaises",lrp);
+
+                //insertamos en el reportview el diseño del ReportDataSource y actualizamos.
                 this.reportViewer1.LocalReport.ReportEmbeddedResource = "ReportePaises.Report1.rdlc";
                 this.reportViewer1.LocalReport.DataSources.Clear();
                 this.reportViewer1.LocalReport.DataSources.Add(rds);
                 this.reportViewer1.RefreshReport();
             }
-            catch (Exception ex)
+            catch (Exception ex)//recoge el error.
             {
                 MessageBox.Show(ex.ToString());
             }
             conn.Close();
+        }
+        /// <summary>
+        /// Metodo que manda envia un MySqlConnection con los datos puesto. para evitar ponerlo varias veces
+        /// </summary>
+        /// <returns> devuelve un MySqlConnection con los datos puesto.</returns>
+        private MySqlConnection getConnection()
+        {
+            String connstr = "server=localhost;user=root;password='';database=world;port=3306";
+            MySqlConnection conn = new MySqlConnection(connstr);
+            return conn;
+
         }
     }
 }
--- ReportePaises/ReportePaises/ReportPaises.cs
+++ ReportePaises/ReportePaises/ReportPaises.cs
@@ -8,9 +8,21 @@ namespace ReportePaises
 {
     internal class ReportPaises
     {
+        /// <summary>
+        /// getter y setter del String code
+        /// </summary>
         public String code { get; set; }
+        /// <summary>
+        /// getter y setter del string nombre
+        /// </summary>
         public String name { get; set; }
+        /// <summary>
+        /// getter y setter del String continent
+        /// </summary>
         public String continent { get; set; }
+        /// <summary>
+        /// constructor
+        /// </summary>
         public ReportPaises() { }
     }
 }
